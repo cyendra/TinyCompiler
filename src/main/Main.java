4668f05dbb8d9e5de3fa70196d796449ea200a90
cyendra <g1483335912@gmail.com>
2014-05-05 21:30:13 +0800

    parser

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,17 @@
 package main;
 
+import java.io.IOException;
+
+import parser.Parser;
+import lexer.Lexer;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		Lexer lex = new Lexer();
+		Parser parse = new Parser(lex);
+		parse.program();
+		System.out.write('\n');
 	}
-
+	
 }
